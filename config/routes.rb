224323c3776e0cacfba860bfd515acb5039f1343
Holly Leary <schleary@gmail.com>
2015-06-05 14:11:44 -0700

    home page and contact links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get  '/',  to: 'home#index',  as: :root
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
